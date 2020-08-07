fc16dca7f8b6f130817596bc5213b48255e591ab
mamanok <rahmansucipto03@gmail.com>
2020-08-07 11:22:59 +0700

    Menambahkan file di github

---
+++ Program Kotlin Part 1/Hello-Kotlin/src/main/kotlin/Functions.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val user = setUser("Rahman", "Drawing", 16)
+    println(user)
+}
+fun setUser(name:String, hobby: String, age:Int) =
+    ("My name is $name, I like $hobby, and I'm $age years old")
+fun printUser (){
+}
